15381e15f67e18080fecda44276e8b5a41719093
MichalinaKa <kaminska.michalinaa@gmail.com>
2020-03-04 18:07:45 +0100

    fetched datas

--- main.js
+++ main.js
@@ -1,51 +1,89 @@
-document.getElementById('button1').addEventListener('click', showDatas);
-document.getElementById('button2').addEventListener('click', showIncomes);
+// document.getElementById('button1').addEventListener('click', fun);
+// document.getElementById('button2').addEventListener('click', showIncomes);
 
 
-function showDatas() {
-    fetch('https://recruitment.hal.skygate.io/companies')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            let html = '';
-            data.forEach(company => {
-                html += `
-                <div class="result--item">
-                    <div class="companies--item">${company.id}</div>
-                    <div class="companies--item">${company.name}</div>
-                    <div class="companies--item">${company.city}</div>
-                </div>
-                `;
+// function showDatas() {
+//     fetch('https://recruitment.hal.skygate.io/companies')
+//         .then(response => {
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log(data);
+//             let html = '';
+//             data.forEach(company => {
+//                 html += `
+//                 <div class="result--item">
+//                     <div class="companies--item">${company.id}</div>
+//                     <div class="companies--item">${company.name}</div>
+//                     <div class="companies--item">${company.city}</div>
+//                 </div>
+//                 `;
 
+//             });
+//             document.getElementById('companies').innerHTML = html;
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         })
+// };
+
+
+// let data = [];
+fetch('https://recruitment.hal.skygate.io/companies')
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.length);
+
+        let display =
+            data.forEach((item, index) => {
+                console.log(`Nr.${index}  ID firmy: ${item.id}, NAZWA FIRMY: ${item.name} PROWADZONA W:  ${item.city} `);
+                fetch("https://recruitment.hal.skygate.io/incomes/" + item.id)
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data2 => {
+                        console.log(data2);
+                    })
+                // let sumIncomes = function
+
+                // let html = '';
+                // html += `
+                //                     <div class="result--item">
+                //                         <div class="companies--item">${item.id}</div>
+                //                         <div class="companies--item">${item.name}</div>
+                //                         <div class="companies--item">${item.city}</div>
+                //                     </div>
+                //  `;
+                //             document.getElementById('companies').innerHTML = html;
             });
-            document.getElementById('companies').innerHTML = html;
-        })
-        .catch(error => {
-            console.log(error);
-        })
-};
 
-function showIncomes() {
-    fetch('https://recruitment.hal.skygate.io/incomes/:id')
-        .then(response => {
-            return response.text();
-        })
-        .then(data => {
-            console.log(data);
-            let html = '';
-            data.forEach(income => {
-                html += `
-                <li>${income.id}</li>
-                <li>${income.value}</li>
-                <li>${income.date}</li>
-                `;
 
-            });
-            document.getElementById('incomes').innerHTML = html;
-        })
-        .catch(error => {
-            console.log(error);
-        })
-}
+    })
+    .catch(error => {
+        console.error('Nie działa link');
+    })
+
+
+// function showIncomes() {
+//     fetch('https://recruitment.hal.skygate.io/incomes/30&34&234')
+//         .then(response => {
+//             return response.text();
+//         })
+//         .then(data => {
+//             console.log(data);
+//             let html = '';
+//             data.forEach(income => {
+//                 html += `
+//                 <li>${income}</li>
+//                 <li>${income.value}</li>
+//                 <li>${income.date}</li>
+//                 `;
+
+//             });
+//             document.getElementById('incomes').innerHTML = html;
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         })}
