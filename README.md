c25435b010747a8c9f84701684e70b0dc6218dd4
MichalinaKa <kaminska.michalinaa@gmail.com>
2020-03-06 17:14:13 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+#Project made like a recrutation task for junior JS position.
+
+##used technologies: html, css, js(ES6)
+
+###Website is displaying information about the comapnies (id, name, total income, city and average income). Datas about the companies are fetched from delivered API 
+- https://recruitment.hal.skygate.io/companies
+- https://recruitment.hal.skygate.io/incomes/:id
+
+###Website is responsive.
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let allData = [];
+const tableHandler = document.getElementById('companies');
 
 fetch('https://recruitment.hal.skygate.io/companies')
     .then(response => {
@@ -7,46 +8,31 @@ fetch('https://recruitment.hal.skygate.io/companies')
     .then(data => {
         console.log(data.length);
         allData = data;
-        // console.log(typeof allData);
-
+        tableHandler.innerHTML = `<div class="container container__headers"><h2 class="container--item blue">id</h2> <h2 class="container--item orange">company name</h2><h2 class="container--item pink">total income</h2><h2 class="container--item green">city</h2><h2 class="container--item coral">average income</h2></div>
+                                               
+`
         let display =
             data.forEach((item, index) => {
-                // console.log(`Nr.${index}  ID firmy: ${item.id}, NAZWA FIRMY: ${item.name} PROWADZONA W:  ${item.city} `);
+
                 fetch("https://recruitment.hal.skygate.io/incomes/" + item.id)
                     .then(response => {
                         return response.json();
                     })
                     .then(data2 => {
-                        // console.log(data2);
                         const arrayOfValues = data2.incomes.map(v => v.value);
                         const totalIncome = arrayOfValues.reduce((a, b) => Number(a) + Number(b));
-
-
                         const sumTotalIncome = Math.floor(totalIncome)
-                        const incomeAverage = sumTotalIncome / 50;
-                        console.log(`Sredni przychód firmy to ${item.name} to ${incomeAverage}`)
+                        const incomeAverage = sumTotalIncome / data2.incomes.length;
 
-
-
-                        // NIE USUWAJ!! to wyświetla datę id przychód;) Typeofweb
-                        const arrayOfDate = data2.incomes.map(v => ({ [v.value]: v.date })).reduce((a, b) => Object.assign(a, b), {});
-                        // console.log(arrayOfDate);
-
-                        let html = `<div class="container container__headers"><h2 class="container--item blue">id</h2> <h2 class="container--item orange">company name</h2><h2 class="container--item pink">total income</h2><h2 class="container--item green">city</h2><h2 class="container--item coral">average income</h2></div>`;
-                        data.forEach(company => {
-                            html += `
+                        tableHandler.innerHTML += `
                         <div class="container">
-                            <div class="container--item blue">${company.id}</div><div class="container--item orange">${company.name}</div>
+                            <div class="container--item blue">${data2.id}</div>
+                            <div class="container--item orange">${item.name}</div>
                             <div class="container--item pink">${sumTotalIncome} </div>
-                            
-                            <div class="container--item green">${company.city}</div>
+                            <div class="container--item green">${item.city}</div>
                             <div class="container--item coral">${incomeAverage}</div>
                         </div>
                         `;
-
-                        });
-                        document.getElementById('companies').innerHTML = html;
-
                     })
 
             });
